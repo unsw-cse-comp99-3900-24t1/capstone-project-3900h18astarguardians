46f37f9c6bc7630288aa0ba7b6d5d84ba7a2eab6
mohammadarsalah <m.arsalah003@gmail.com>
2024-05-18 19:15:44 +1000

    fixed spacing of register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -57,7 +57,7 @@ const Register = () => {
         <CssBaseline />
         <Box
           sx={{
-            marginTop: 8,
+            marginTop: 2,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
