69b33f30fee6a316ec095d9e47aed002e9aecc4f
mohammadarsalah <m.arsalah003@gmail.com>
2024-05-22 19:51:24 +1000

    did some things

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -11,7 +11,7 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
+import Container from "@dmui/material/Container";
 
 const Register = () => {
   const { displayError, displaySuccess } = useContext(UserContext);
