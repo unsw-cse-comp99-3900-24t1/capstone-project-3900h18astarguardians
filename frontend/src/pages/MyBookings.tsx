74cf5fc1213c980c0938cb9174eb8bde9c96d1e0
Nasser Malibari <nassermalibari1@gmail.com>
2024-06-20 19:49:33 +1000

    initial req

--- frontend/src/pages/MyBookings.tsx
+++ frontend/src/pages/MyBookings.tsx
@@ -1,9 +1,43 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+import { request } from "../utils/axios";
 
 
-const MyBookings  = () => {  
-    
-    return <h1> Bookings here </h1>
+
+const MyBookings  = () => {
+    // fetch data
+    const [bookings, setBookings] = useState('');
+
+
+    const navigate = useNavigate();
+    const checkLoggedIn = async () => {
+        try {
+        await request.get("/users/showMe");
+        } catch (e) {
+        console.log(e);
+        }
+    };
+    const getBookings = async () => {
+        try {
+            const resp = await request.get("/bookings/showAllMyBookings");
+            const data = resp.data;
+            console.log(data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    useEffect(() => {
+        checkLoggedIn();
+        getBookings();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return <>
+        <h1> |{bookings}| </h1>
+        <h1> hi </h1>
+    </>
+    // <h1> Bookings here </h1>
 };
 
 export default MyBookings;
