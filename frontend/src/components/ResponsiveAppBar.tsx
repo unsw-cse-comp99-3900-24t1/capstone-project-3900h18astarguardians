e26d8c9487d1c0a65a489ac3a77ec7b5f86e8c82
Nasser Malibari <nassermalibari1@gmail.com>
2024-07-27 13:43:22 +1000

    app bar fixes

--- frontend/src/components/ResponsiveAppBar.tsx
+++ frontend/src/components/ResponsiveAppBar.tsx
@@ -1,57 +1,91 @@
-import { useState, MouseEvent, useEffect } from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../utils/context";
+import { AxiosError } from 'axios';
 import { request } from "../utils/axios";
-import { AxiosError } from "axios";
-import NotificationSettingsModal from '../components/NotificationSettingsModal'
+import { useEffect, useState } from 'react';
+import NotificationSettingsModal from '../components/NotificationSettingsModal';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
-const pages = ["login"];
-const settings = ["dashboard", "profile", "notification setting"];
-const logged_in_pages = ["MyBookings", "contact"]
-const admin_pages = ["requests", "reports"]
-
-const ResponsiveAppBar = () => {
-  const [anchorElNav, setAnchorElNav] = useState<HTMLElement | null>(null);
-  const [anchorElUser, setAnchorElUser] = useState<HTMLElement | null>(null);
+function ResponsiveAppBar() {
+  const navigate = useNavigate();
   const [notificationSettingsOpen, setNotificationSettingsOpen] = useState(false);
   const [emailState, setEmailState] = useState({
     hasConfirmationEmail: true,
     hasNotificationEmail: true,
   });
-
-  // fn's for opening and closing
-  const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) =>
-    setAnchorElNav(event.currentTarget);
-  const handleOpenUserMenu = (event: MouseEvent<HTMLElement>) =>
-    setAnchorElUser(event.currentTarget);
-  const handleCloseNavMenu = () => setAnchorElNav(null);
-  const handleCloseUserMenu = () => setAnchorElUser(null);
-  // changeNotificationSettingsModal
+  let pages = [];
+  const { token, removeToken, displaySuccess, displayError, removeAdmin } = useGlobalContext();
+  const handleCloseNotificationSettingsModal = () => setNotificationSettingsOpen(false);
   const handleOpenNotificationSettingsModal = () => {
-    handleCloseUserMenu()
-    setNotificationSettingsOpen(true)
-  }
-  const handleCloseNotificationSettingsModal = () => setNotificationSettingsOpen(false)
+        handleCloseUserMenu()
+        setNotificationSettingsOpen(true)
+      }
 
-  const { token, removeToken, displaySuccess, displayError, admin, removeAdmin } =
-    useGlobalContext();
+  const handleLogout = async () => {
+    try {
+      await request.get("/auth/logout");
+      removeToken();
+      removeAdmin();
+      navigate("/login");
+      displaySuccess("Successfully Logged out");
+      localStorage.removeItem('filterConfig');
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        const msg = err.response!.data.error;
+        displayError(msg);
+      }
+    }
+  };
+  if (token !== null) {
+    pages.push('MyBookings');
+    pages.push('contact');
+  } else {
+    pages.push('login');
+  }
+
+  if (token !== null && token.type === "admin") {
+    pages.push('requests');
+    pages.push('reports');
+  }
 
   useEffect(() => {
     getUserInfo()
   }, [])
+  
+  // const pages = ['login', 'MyBookings', 'contact', 'requests','reports'];
+  const settings = ['Dashboard'];
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   const getUserInfo = async () => {
     try {
       const userInfoData = await request.get("/users/showMe");
@@ -87,27 +121,13 @@ const ResponsiveAppBar = () => {
     }
   }
 
-  const navigate = useNavigate();
-  const handleLogout = async () => {
-    try {
-      await request.get("/auth/logout");
-      removeToken();
-      removeAdmin();
-      navigate("/login");
-      displaySuccess("Successfully Logged out");
-      localStorage.removeItem('filterConfig');
-    } catch (err) {
-      if (err instanceof AxiosError) {
-        const msg = err.response!.data.error;
-        displayError(msg);
-      }
-    }
-  };
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+           */}
+           <AppRegistrationIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}/>
           <Typography
             variant="h6"
             noWrap
@@ -115,17 +135,18 @@ const ResponsiveAppBar = () => {
             href="/dashboard"
             sx={{
               mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
               fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
             }}
           >
-            LOGO
+            CSE-ROOMS
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -140,165 +161,95 @@ const ResponsiveAppBar = () => {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
+                vertical: 'bottom',
+                horizontal: 'left',
               }}
               keepMounted
               transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
+                vertical: 'top',
+                horizontal: 'left',
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: "block", md: "none" },
+                display: { xs: 'block', md: 'none' },
               }}
             >
               {pages.map((page) => (
-                <MenuItem
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  component={RouterLink}
-                  to={`/${page}`}
-                >
-                  <Typography
-                    textAlign="center"
-                    sx={{ textTransform: "capitalize" }}
-                  >
-                    {page}
-                  </Typography>
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/dashboard"
             sx={{
               mr: 2,
-              display: { xs: "flex", md: "none" },
+              display: { xs: 'flex', md: 'none' },
               flexGrow: 1,
-              fontFamily: "monospace",
+              fontFamily: 'monospace',
               fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
             }}
           >
             LOGO
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {!token &&
-              pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{
-                    my: 1,
-                    display: "block",
-                    color: "white",
-                    outline: "2px solid white",
-                    mx: 1,
-                  }}
-                  variant="contained"
-                  component={RouterLink}
-                  to={`/${page}`}
-                >
-                  {page}
-                </Button>
-              ))}
-            {token &&
-              logged_in_pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{
-                    my: 1,
-                    display: "block",
-                    color: "white",
-                    outline: "2px solid white",
-                    mx: 1,
-                  }}
-                  variant="contained"
-                  component={RouterLink}
-                  to={`/${page}`}
-                >
-                  {page}
-                </Button>
-              ))}
-              {admin &&
-              admin_pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{
-                    my: 1,
-                    display: "block",
-                    color: "white",
-                    outline: "2px solid white",
-                    mx: 1,
-                  }}
-                  variant="contained"
-                  component={RouterLink}
-                  to={`/${page}`}
-                >
-                  {page}
-                </Button>
-              ))}
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+                component={RouterLink}
+                to={`/${page}`}
+              >
+                {page}
+              </Button>
+            ))}
           </Box>
+
           <Box sx={{ flexGrow: 0 }}>
-            {token && (
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar
-                    alt="profile picture"
-                    // src="../../public/default-profile-pic"
-                  />
-                </IconButton>
-              </Tooltip>
-            )}
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} size="large" color="inherit" >
+                <AccountCircle />
+                {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
+              </IconButton>
+            </Tooltip>
             <Menu
-              sx={{ mt: "45px" }}
+              sx={{ mt: '45px' }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
+                vertical: 'top',
+                horizontal: 'right',
               }}
               keepMounted
               transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
+                vertical: 'top',
+                horizontal: 'right',
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
               {settings.map((setting) => (
-                setting !== 'notification setting'? (<MenuItem
-                  sx={{ textTransform: "capitalize" }}
-                  key={setting}
-                  onClick={handleCloseUserMenu}
-                  component={RouterLink}
-                  to={`/${setting}`}
-                >
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
                   <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>):(<MenuItem
-                  sx={{ textTransform: "capitalize" }}
-                  key={setting}
-                  onClick={handleOpenNotificationSettingsModal}
-                >
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>)
+                </MenuItem>
               ))}
               <MenuItem
-                onClick={() => {
-                  handleLogout();
-                  handleCloseUserMenu();
-                }}
-              >
+                  sx={{ textTransform: "capitalize" }}
+                  onClick={handleOpenNotificationSettingsModal}
+                >
+                  <Typography textAlign="center">Settings</Typography>
+                </MenuItem>
+              <MenuItem onClick={handleLogout}>
                 <Typography textAlign="center">Logout</Typography>
               </MenuItem>
             </Menu>
@@ -314,5 +265,5 @@ const ResponsiveAppBar = () => {
       </Container>
     </AppBar>
   );
-};
+}
 export default ResponsiveAppBar;
