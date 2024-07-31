97eac6e7ac2fea4c4cb2ef4d07b68d15e25137dc
mohammadarsalah <m.arsalah003@gmail.com>
2024-07-31 11:12:27 +1000

    added setup for backend api testing

--- backend/__tests__/auth.test.ts
+++ backend/__tests__/auth.test.ts
@@ -1,9 +1,11 @@
+// auth.test.ts
+
 import request from "supertest";
 import app from "../app";
 
 import { config } from "dotenv";
 import mongoose from "mongoose";
-
+config();
 const { MONGO_URI } = process.env;
 
 // connect to db
@@ -11,11 +13,18 @@ beforeEach(async () => {
   await mongoose.connect(MONGO_URI as string);
 });
 
-describe("successfully register and login user", () => {
-  it("should successfully login", async () => {
+describe("register and login user", () => {
+  it("should successfull login", async () => {
     const credentials = { email: "m.arsalah003@gmail.com" };
     const res = await request(app).post("/api/v1/auth/login").send(credentials);
     expect(res.statusCode).toBe(200);
+    console.log(res.body);
+  });
+
+  it("should unsuccessfully login", async () => {
+    const credentials = { email: "blah@gmail.com" };
+    const res = await request(app).post("/api/v1/auth/login").send(credentials);
+    expect(res.statusCode).toBe(401);
     console.log(res.body);
   });
 });
